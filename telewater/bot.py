eb215222640ece61bbb9b4f5929bbe59048b1311
nasridho <rmsyfa@gmail.com>
2022-03-14 13:51:13 +0700

    messok

--- telewater/bot.py
+++ telewater/bot.py
@@ -100,7 +100,7 @@ async def watermarker(event):
 
     org_file = stamp(await event.download_media(""), user=str(event.sender_id))
 
-    file = File("image.png")
+    file = File("messi.mp4")
     wtm = Watermark(File(org_file), pos=conf.config.position)
 
     out_file = apply_watermark(
